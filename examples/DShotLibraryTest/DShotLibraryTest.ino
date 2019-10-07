00681c24d9f8596768d0f09a04e4ac3378177605
Andy Tsui <gueei.com@gmail.com>
2019-10-08 05:19:30 +0800

    Implementing DShot600 with multiple ESCs / Not tested yet 	modified:   examples/DShotLibraryTest/DShotLibraryTest.ino 	new file:   keywords.txt 	modified:   src/DShot.cpp 	modified:   src/DShot.h

--- examples/DShotLibraryTest/DShotLibraryTest.ino
+++ examples/DShotLibraryTest/DShotLibraryTest.ino
@@ -1,15 +1,32 @@
 #include <DShot.h>
 
-DShot esc(7);
+/*
+
+redefine DSHOT_PORT if you want to change the default PORT
+
+Defaults
+UNO: PORTD, available pins 0-7 (D0-D7)
+Leonardo: PORTB, available pins 4-7 (D8-D11)
+
+e.g.
+#define DSHOT_PORT PORTD
+*/
+DShot esc1, esc2;
 
 void setup() {
   Serial.begin(115200);
+
+  // Notice, all pins must be connected to same PORT
+  esc1.attach(7);
+  esc2.attach(5);
+  esc1.setThrottle(0xf0f);
+  esc2.setThrottle(0xfff);
 }
 
 void loop() {
   if (Serial.available()>0){
     uint16_t throttle = Serial.parseInt();
-    uint16_t packet = esc.setThrottle(throttle);
+    uint16_t packet = esc1.setThrottle(throttle);
     Serial.print(throttle, HEX);
     Serial.print("\t");
     Serial.println(packet, HEX);
--- src/DShot.h
+++ src/DShot.h
@@ -2,34 +2,26 @@
 
 #ifndef DShot_h
 #define DShot_h
-// Bunch of NOP to pause the CPU
-#define NOP asm("nop\n")
-#define NOP2 NOP;NOP
-#define NOP4 NOP2;NOP2
-#define NOP8 NOP4;NOP4
-#define NOP16 NOP8;NOP8
-#define NOP32 NOP16;NOP16
 
 #if defined(__AVR_ATmega328P__)
 // For UNO, PortD 0-7: i.e. D0-D7
-#define OUTPUT_PORT PORTD
+#define DSHOT_PORT PORTD
 #endif
 
 #if defined(__AVR_ATmega32U4__)
-// We are using Digital pin 9: PORTB5
-#define OUTPUT_PORT PORTB
+// For Leonardo, PortB 4-7: i.e. D8-D11
+#define DSHOT_PORT PORTB
 #endif
 
 class DShot{
   public:
-    DShot(uint8_t pin);
+    DShot();
+    void attach(uint8_t pin);
     uint16_t setThrottle(uint16_t throttle);
   private:
     uint16_t _packet = 0;
     uint16_t _throttle = 0;
-    uint8_t _pin = 0;
-    void dShot300_0();
-    void dShot300_1();
+    uint8_t _pinMask = 0;
 };
 
 #endif
