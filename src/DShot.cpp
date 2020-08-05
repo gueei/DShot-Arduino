cdbab1f4d4f38532b8866469ca8f586d425471d0
Aleksey Ivanov <aleksey.ivanov@iqoption.com>
2020-08-05 15:16:56 +0300

    Added modes

--- src/DShot.cpp
+++ src/DShot.cpp
@@ -10,6 +10,9 @@ static uint8_t dShotBits[16];
 // Denote which pin is attached to dShot
 static uint8_t dShotPins = 0;
 
+// Mode: 600/300/150
+static enum DShot::Mode dShotMode = DShot::Mode::DSHOT600;
+
 
 #define NOP	"NOP\n"
 #define NOP2 NOP NOP
@@ -25,38 +28,176 @@ static uint8_t dShotPins = 0;
 */
 static inline void sendData(){
 	noInterrupts();
-	asm(
+	switch (dShotMode) {
+	case DShot::Mode::DSHOT600:
+	default:
+		asm(
 		// For i = 0 to 15:
-		"LDI r23, 0	\n"
+		"LDI	r23,	0\n"
 		// Set High for every attached pins
 		// DSHOT_PORT |= dShotPins;
-		"IN r25, %0 \n"
-		"_for_loop:			\n"
-		"OR r25, %1 \n"
-		"OUT %0, r25 \n"
-		// Wait 10 cycles
+		"IN	r25,	%0\n"
+		"_for_loop_0:\n"
+		"OR	r25,	%1\n"
+		// Wait 7 cycles (7 - 6 = 1)
+		"NOP\n"
+
+		"OUT	%0,	r25\n"
+		// Wait 10 cycles (10 - 4 = 6)
 		NOP4
 		NOP2
 		// Set Low for low bits only
 		//DSHOT_PORT &= dShotBits[i];
-		"LD r24, Z+		\n"
-		"AND r25, r24	\n"
-		"OUT %0, r25	\n"
-		// Wait 10 cycles
+		"LD	r24,	Z+\n"
+		"AND	r25,	r24\n"
+		"OUT	%0,	r25\n"
+		// Wait 10 cycles (10 - 2 = 8)
 		NOP8
 		// Turn off everything
 		// DSHOT_PORT &= ~dShotPins;
-		"AND r25, %2 \n"
-		"OUT %0, r25 \n"
+		"AND	r25,	%2\n"
+		"OUT	%0,	r25\n"
 		// Add to i (tmp_reg)
-		"INC r23		\n"
-		"CPI r23, 16	\n"
-		"BRLO	_for_loop	\n"
+		"INC	r23\n"
+		"CPI	r23,	16\n"
+		"BRLO	_for_loop_0\n"
 		// 7 cycles to next bit (4 to add to i and branch, 2 to turn on), no wait
 		:
 		: "I" (_SFR_IO_ADDR(DSHOT_PORT)), "r" (dShotPins), "r" (~dShotPins), "z" (dShotBits)
 		: "r25", "r24", "r23"
-	);
+		);
+		break;
+	case DShot::Mode::DSHOT300:
+		asm(
+		// For i = 0 to 15:
+		"LDI	r23,	0\n"
+		// Set High for every attached pins
+		// DSHOT_PORT |= dShotPins;
+		"IN	r25,	%0\n"
+		"_for_loop_1:\n"
+		"OR	r25,	%1\n"
+		// Wait 14 cycles (14 - 6 = 8)
+
+		// 1 + 3 * N //
+		"LDI	r26,	2\n"		// 1 // set N
+		"_sleep_loop_1_1:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_1_1\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+		"NOP\n"
+
+		"OUT	%0,	r25\n"
+		// Wait 20 cycles (20 - 4 = 16)
+
+		// 1 + 3 * N //
+		"LDI	r26,	5\n"		// 1 // set N
+		"_sleep_loop_1_2:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_1_2\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+
+		// Set Low for low bits only
+		//DSHOT_PORT &= dShotBits[i];
+		"LD	r24,	Z+\n"	// 2 //
+		"AND	r25,	r24\n"
+		"OUT	%0,	r25\n"
+		// Wait 20 cycles (20 - 2 = 18)
+
+		// 1 + 3 * N //
+		"LDI	r26,	5\n"		// 1 // set N
+		"_sleep_loop_1_3:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_1_3\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+
+		"NOP\n"
+		"NOP\n"
+		// Turn off everything
+		// DSHOT_PORT &= ~dShotPins;
+		"AND	r25,	%2\n"
+		"OUT	%0,	r25\n"
+		// Add to i (tmp_reg)
+		"INC	r23\n"
+		"CPI	r23,	16\n"
+		"BRLO	_for_loop_1\n"
+		// 7 cycles to next bit (4 to add to i and branch, 2 to turn on), no wait
+		:
+		: "I" (_SFR_IO_ADDR(DSHOT_PORT)), "r" (dShotPins), "r" (~dShotPins), "z" (dShotBits)
+		: "r25", "r24", "r23"
+		);
+		break;
+	case DShot::Mode::DSHOT150:
+		asm(
+		// For i = 0 to 15:
+		"LDI	r23,	0\n"
+		// Set High for every attached pins
+		// DSHOT_PORT |= dShotPins;
+		"IN	r25,	%0\n"
+
+		"_for_loop_2:\n"
+		"OR	r25,	%1\n"
+		// Wait 28 cucles (28 - 6 = 22)
+
+		// 1 + 3 * N //
+		"LDI	r26,	7\n"		// 1 // set N
+		"_sleep_loop_2_1:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_2_1\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+
+		"OUT	%0,	r25\n"
+		// Wait 40 cycles (40 - 4 = 36)
+
+		// 1 + 3 * N //
+		"LDI	r26,	11\n"		// 1 // set N
+		"_sleep_loop_2_2:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_2_2\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+
+		"NOP\n"
+		"NOP\n"
+
+		// Set Low for low bits only
+		//DSHOT_PORT &= dShotBits[i];
+		"LD	r24,	Z+\n"
+		"AND	r25,	r24\n"
+		"OUT	%0,	r25\n"
+		// Wait 40 cycles (40 - 2 = 38)
+
+		// 1 + 3 * N //
+		"LDI	r26,	11\n"		// 1 // set N
+		"_sleep_loop_2_3:\n"
+		"DEC	r26\n"			// 1 //
+		"BRNE	_sleep_loop_2_3\n"	// 2 //
+		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		// 1 + 3 * N //
+
+		"NOP\n"
+		"NOP\n"
+		"NOP\n"
+		"NOP\n"
+
+		// Turn off everything
+		// DSHOT_PORT &= ~dShotPins;
+		"AND	r25,	%2\n"
+		"OUT	%0,	r25\n"
+		// Add to i (tmp_reg)
+		"INC	r23\n"
+		"CPI	r23,	16\n"
+		"BRLO	_for_loop_2\n"
+		// 7 cycles to next bit (4 to add to i and branch, 2 to turn on), no wait
+		:
+		: "I" (_SFR_IO_ADDR(DSHOT_PORT)), "r" (dShotPins), "r" (~dShotPins), "z" (dShotBits)
+		: "r25", "r24", "r23", "r26"
+		);
+		break;
+	}
 	interrupts();
 }
 
@@ -82,8 +223,8 @@ TIMSK1 |= (1 << OCIE1A);
 	timerActive = true;
 	for (byte i=0; i<16; i++){
 		dShotBits[i] = 0;
-		dShotPins = 0;
 	}
+	dShotPins = 0;
 
 	sei(); // allow interrupts
 }
@@ -104,7 +245,7 @@ static inline uint16_t createPacket(uint16_t throttle){
   uint8_t csum = 0;
   throttle <<= 1;
 	// Indicate as command if less than 48
-	if (throttle <48)
+	if (throttle < 48 && throttle > 0)
 		throttle |= 1;
   uint16_t csum_data = throttle;
   for (byte i=0; i<3; i++){
@@ -117,7 +258,8 @@ static inline uint16_t createPacket(uint16_t throttle){
 
 /****************** end of static functions *******************/
 
-DShot::DShot(){
+DShot::DShot(const enum Mode mode){
+    dShotMode = mode;
 }
 
 void DShot::attach(uint8_t pin){
